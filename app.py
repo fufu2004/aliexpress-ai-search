db87213cad26d3d9762e294eb7e04d65646812e7
fufu2004 <110229979+fufu2004@users.noreply.github.com>
2025-08-01 19:08:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,12 @@ from search import search_aliexpress
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
-def home():
-    results = []
-    if request.method == "POST":
-        q = request.form.get("query")
-        data = search_aliexpress(q)
-        results = data.get("result", {}).get("products", [])
-    return render_template("index.html", results=results)
+def index():
+    if request.method == 'POST':
+        query = request.form['query']
+        results = search_aliexpress(query)
+        return render_template('index.html', query=query, results=results)
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
